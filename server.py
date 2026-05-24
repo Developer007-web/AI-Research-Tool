f73de82155699e90f84a380c4473f4d35e003657
Aman Pratap Singh <amanprataps218@gmail.com>
2026-05-24 14:54:45 +0530

    Update server.py

--- server.py
+++ server.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
 
     mcp.run(
-        transport="http",
+        transport="sse",
         host="0.0.0.0",
         port=port
     )
